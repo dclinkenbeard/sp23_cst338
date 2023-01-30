7fb0393de4391551949a44ee920283f98860d2e4
daclink <drew@daclink.com>
2023-01-30 13:28:16 -0800

    Added the next slide.

--- src/main/java/week02/JavaTour.java
+++ src/main/java/week02/JavaTour.java
@@ -6,5 +6,13 @@ public class JavaTour {
   public JavaTour() {
     x = 23;
   }
+
+  private void evaluateX(){
+    if(x < 42){
+      System.out.println("x is less than 42 ( x == " + x +" )");
+    }else if(x > 42){
+      System.out.println("x is greater than 42 ( x == " + x +" )");
+    }
+  }
 }
 
